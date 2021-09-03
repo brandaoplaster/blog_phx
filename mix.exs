ef5d0b1f30820da9b6e141afb85a1767dec06fdd
brandaoplaster <brandaoplaster@gmail.com>
2021-09-02 23:39:07 -0300

    feat: Adding CI quality

--- mix.exs
+++ mix.exs
@@ -16,7 +16,8 @@ defmodule BlogPhx.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.json": :test
       ]
     ]
   end
