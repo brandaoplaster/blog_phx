92f4a1ce4cc3c35d6e7133ada98edc0e1dd54aab
brandaoplaster <brandaoplaster@gmail.com>
2021-09-02 21:22:14 -0300

    chore: installing sobelow and created configuration

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule BlogPhx.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
