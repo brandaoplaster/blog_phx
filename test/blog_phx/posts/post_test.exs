62225a8dc009dd1809332f43cb53e0492e8bc5d5
brandaoplaster <brandaoplaster@gmail.com>
2021-09-19 20:45:03 -0300

    chore: Adding test for list and get post

--- test/blog_phx/posts/post_test.exs
+++ test/blog_phx/posts/post_test.exs
@@ -10,9 +10,24 @@ defmodule BlogPhx.Posts.PostTest do
     description: "Lorem..."
   }
 
+  def post_fixture(_attrs \\ %{}) do
+    {:ok, post} = Posts.create_post(@valid_post)
+    post
+  end
+
   test "create_post/1 with valid params" do
     assert {:ok, %Post{} = post} = Posts.create_post(@valid_post)
     assert post.title == "Rails"
     assert post.description == "Lorem..."
+  end
+
+  test "list_post/0 return all posts" do
+    post = post_fixture()
+    assert Posts.list_post() == [post]
+  end
+
+  test "get_post/1 return all posts" do
+    post = post_fixture()
+    assert Posts.get_post(post.id) == post
   end
 end
