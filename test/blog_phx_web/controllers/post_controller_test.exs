1081a1718e7782fe44c09a01e795295937fd9eab
brandaoplaster <brandaoplaster@gmail.com>
2021-09-19 22:07:15 -0300

    chore: Adding test to show and new post controller

--- test/blog_phx_web/controllers/post_controller_test.exs
+++ test/blog_phx_web/controllers/post_controller_test.exs
@@ -14,4 +14,15 @@ defmodule BlogPhxWeb.PostControllerTest do
     conn = get(conn, Routes.post_path(conn, :index))
     assert html_response(conn, 200) =~ "Rails"
   end
+
+  test "action :show for get post by id", %{conn: conn} do
+    {:ok, post} = Posts.create_post(@valid_post)
+    conn = get(conn, Routes.post_path(conn, :show, post))
+    assert html_response(conn, 200) =~ "Rails"
+  end
+
+  test "action :new for new posts", %{conn: conn} do
+    conn = get(conn, Routes.post_path(conn, :new))
+    assert html_response(conn, 200) =~ "Create Post"
+  end
 end
