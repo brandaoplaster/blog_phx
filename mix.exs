35f46d4e83664bb6e1ad2751b4e7b6a3be00af02
brandaoplaster <brandaoplaster@gmail.com>
2021-11-02 17:19:22 -0300

    chore: installing ueberauth and adding environment variables

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule BlogPhx.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:sobelow, "~> 0.8", only: :dev},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ueberauth_google, "~> 0.10"}
     ]
   end
 
