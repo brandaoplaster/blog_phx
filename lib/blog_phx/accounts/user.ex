0cadcf2a191c5bb310ad856520a1fdfdf34f3b43
brandaoplaster <brandaoplaster@gmail.com>
2021-11-03 21:33:47 -0300

    chore: Adding migrate and schema for user

---
+++ lib/blog_phx/accounts/user.ex
@@ -0,0 +1,23 @@
+defmodule BlogPhx.Accounts.User do
+  @moduledoc false
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :first_name, :string
+    field :image, :string
+    field :last_name, :string
+    field :provider, :string
+    field :token, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:first_name, :last_name, :image, :provider, :email, :token])
+    |> validate_required([:first_name, :last_name, :image, :provider, :email, :token])
+  end
+end
